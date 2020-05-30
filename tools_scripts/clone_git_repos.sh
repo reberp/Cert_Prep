2676ec7b8324fc7d02f87cc94f87bee8c3d4a51f
Pat <pat@email.com>
2020-05-30 06:07:44 -0400

    meant to add

---
+++ tools_scripts/clone_git_repos.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+git clone https://github.com/carlospolop/privilege-escalation-awesome-scripts-suite
+git clone https://github.com/SecureAuthCorp/impacket
+git clone https://github.com/swisskyrepo/PayloadsAllTheThings
+git clone https://github.com/sleventyeleven/linuxprivchecker
+git clone https://github.com/pentestmonkey/unix-privesc-check
+git clone https://github.com/rebootuser/LinEnum
+git clone https://github.com/mattiareggiani/WinEnum
+git clone https://github.com/samratashok/nishang
